b8ba8f41f5fd3ffaf412e1b3aa2a2202c4a862f6
cole <cole.richardson@mail.utoronto.ca>
2020-04-30 17:08:41 -0400

    starter code for selenium

---
+++ getlichesslink.py
@@ -0,0 +1,15 @@
+import requests
+from selenium.webdriver import Chrome
+from selenium.webdriver.chrome.options import Options
+
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+
+url = "https://lichess.org/"
+
+webdriver = #path to webdriver
+#"C:/Users/ColeR/Documents/chromedriver.exe"
+driver = Chrome(options=chrome_options, executable_path=webdriver)
+driver.get(url)
+button = driver.find_element_by_id('SummonerRefreshButton')
+button.click()
